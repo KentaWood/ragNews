40e5d197ba9d6d8e6707ebdbff1b9028483b89c3
Alexander Wood <115967095+KentaWood@users.noreply.github.com>
2024-09-16 11:13:51 -0700

    working state of the RAGnews

--- ragNews.py
+++ ragNews.py
@@ -9,16 +9,16 @@ and the path to the database can be changed with the --db parameter.
 
 from cmd import PROMPT
 from email import parser
-# from bs4 import BeautifulSoup
+
 from urllib.parse import urlparse
 import datetime
 import logging
 import re
-# import requests
+
 import sqlite3
 
 import groq
-# import metahtml
+
 
 from groq import Groq
 import os
@@ -71,22 +71,18 @@ def extract_keywords(text, seed=None):
     this function extracts the keywords that will be used to perform the search for articles that will be used in RAG.
 
     >>> extract_keywords('Who is the current democratic presidential nominee?', seed=0)
-    'Joe Biden Presidential Democratic Nominee'
+    'Joe Biden nomination Kamala Harris election politics'
+    
     >>> extract_keywords('What is the policy position of Trump related to illegal Mexican immigrants?', seed=0)
-    'Trump Mexico border immigrants illegal immigration enforcement deportation'
+    'Trump border immigration Mexican illegal immigrants policy'
 
     Note that the examples above are passing in a seed value for deterministic results.
     In production, you probably do not want to specify the seed.
     '''
 
-    # FIXME:
-    # Implement this function.
-    # It's okay if you don't get the exact same keywords as me.
-    # You probably certainly won't because you probably won't come up with the exact same prompt as me.
-    # To make the test cases above pass,
-    # you'll have to modify them to be what the output of your prompt provides.
 
-    system = "What are ten words about this topics or question, give only the keywords of this, get rid of \",\" and \"Here are the ten keywords:\" and write them all in one line with a space inbetween each word in the output"
+    system = "Extract ten keywords related to this topic or question. Remove commas and any introductory text, and present the words in a single line separated by spaces. I dont want \"Here are the keywords related to the topic:\n\n\".I dont care about anything else"
+    print("extract_keywords:")
     return run_llm(system,text,seed=seed)
 
 ################################################################################
@@ -129,6 +125,7 @@ def rag(text, db):
     # Recall that your RAG system should:
     # 1. Extract keywords from the text.
     # 2. Use those keywords to find articles related to the text.
+    # using the keywords find the article by 
     # 3. Construct a new user prompt that includes all of the articles and the original text.
     # 4. Pass the new prompt to the LLM and return the result.
     #
@@ -138,35 +135,69 @@ def rag(text, db):
     # You can start with a basic system prompt right away just to check if things are working,
     # but don't spend a lot of time on the system prompt until you're sure everything else is working.
     # Then, you can iteratively add more commands into the system prompt to correct "bad" behavior you see in your program's output.
+    
+    # Instantiate the ArticleDB object
+    
+
+    # Extract keywords from the input text
+    keywords = extract_keywords(text)
+
+    print(keywords)
+    # Find articles related to the keywords
+    articles = db.find_articles(keywords,limit=5)
+    
+    # Concatenate the article content
+    article_content = "\n".join(article['text'] for article in articles)
+
+    
+
+    # Load the system prompt from a file (e.g., 'prompt.txt')
+    prompt = "You're an expert at providing concise answers based solely on article summaries. Below is the user's query and relevant article summaries. Answer the query directly using only the information from the summaries. If the information isn't clear, reanalyze the summaries to find the answer. Always respond based on the data available, avoiding phrases like 'The summaries do not mention...' You are a professional journalist tasked with answering a reader's question using the provided articles for context."
+
+    # Create the full prompt by combining the user input and relevant articles
+    full_prompt = f"{text}\n\nRelevant Articles:\n{article_content}"
+    
+    print("RAG")
+    # Pass the system prompt and full prompt to the LLM and return the result
+    return run_llm(prompt, full_prompt)
+    
+    
+           
+    
+    
+    
+    
+    
+    
 
 
 class ArticleDB:
-    '''
-    This class represents a database of news articles.
-    It is backed by sqlite3 and designed to have no external dependencies and be easy to understand.
+    # '''
+    # This class represents a database of news articles.
+    # It is backed by sqlite3 and designed to have no external dependencies and be easy to understand.
 
-    The following example shows how to add urls to the database.
+    # The following example shows how to add urls to the database.
 
-    >>> db = ArticleDB()
-    >>> len(db)
-    0
-    >>> db.add_url(ArticleDB._TESTURLS[0])
-    >>> len(db)
-    1
+    # >>> db = ArticleDB()
+    # >>> len(db)
+    # 0
+    # >>> db.add_url(ArticleDB._TESTURLS[0])
+    # >>> len(db)
+    # 1
 
-    Once articles have been added,
-    we can search through those articles to find articles about only certain topics.
+    # Once articles have been added,
+    # we can search through those articles to find articles about only certain topics.
 
-    >>> articles = db.find_articles('Economía')
+    # >>> articles = db.find_articles('Economía')
 
-    The output is a list of articles that match the search query.
-    Each article is represented by a dictionary with a number of fields about the article.
+    # The output is a list of articles that match the search query.
+    # Each article is represented by a dictionary with a number of fields about the article.
 
-    >>> articles[0]['title']
-    'La creación de empleo defrauda en Estados Unidos en agosto y aviva el temor a una recesión | Economía | EL PAÍS'
-    >>> articles[0].keys()
-    ['rowid', 'rank', 'title', 'publish_date', 'hostname', 'url', 'staleness', 'timebias', 'en_summary', 'text']
-    '''
+    # >>> articles[0]['title']
+    # 'La creación de empleo defrauda en Estados Unidos en agosto y aviva el temor a una recesión | Economía | EL PAÍS'
+    # >>> articles[0].keys()
+    # ['rowid', 'rank', 'title', 'publish_date', 'hostname', 'url', 'staleness', 'timebias', 'en_summary', 'text']
+    # '''
 
     _TESTURLS = [
         'https://elpais.com/economia/2024-09-06/la-creacion-de-empleo-defrauda-en-estados-unidos-en-agosto-y-aviva-el-fantasma-de-la-recesion.html',
@@ -215,47 +246,75 @@ class ArticleDB:
     def find_articles(self, query, limit=10, timebias_alpha=1):
         '''
         Return a list of articles in the database that match the specified query.
-
+        
         Lowering the value of the timebias_alpha parameter will result in the time becoming more influential.
         The final ranking is computed by the FTS5 rank * timebias_alpha / (days since article publication + timebias_alpha).
         '''
+        # >>> test = ArticleDB('ragnews.db')
+        # >>> test.find_articles(query='trump harris debate',limit=2)
+        # ['Las reglas para el próximo debate Harris vs. Trump despiertan controversia - Video', "Gov. Sanders: Tuesday's debate more important for Harris than Trump - ABC News"]
+        # >>> test.find_articles(query=' ')
+        # []
+        # '''
         
-        # FIXME:
-        # Implement this function.
-        # You do not need to concern yourself with the timebias_alpha parameter.
-        # (Although I encourage you to try!)
-        #
-        # HINT:
-        # The only thing my solution does is pass a SELECT statement to the sqlite3 database.
-        # The SELECT statement will need to use sqlite3's FTS5 syntax for full text search.
-        # If you need to review how to coordinate sqlite3 and python,
-        # there is an example in the __len__ method below.
-        # The details of the SELECT statement will be different
-        # (because the functions collect different information)
-        # but the outline of the python code is the same.
+        # Split the query into a list of individual words
+        query_list = query.split()
+
+        # Join the query list into a string with ' OR ' to construct the OR query
+        query = ' OR '.join(query_list)
+
+        # Print the constructed query
+        print('query:', query)
+
+        # Define the SQL query
+        sql = '''
+            SELECT rowid, rank, title, publish_date, hostname, url, en_summary, text
+            FROM articles
+            WHERE articles MATCH ?
+            ORDER BY rank 
+            LIMIT ?
+        '''
+
+        # Execute the query with the provided limit
+        result = self.db.execute(sql, (query, limit)).fetchall()
+
+        # Convert the result rows to dictionaries with only 'title' and 'en_summary'
+        articles = [{'title': row['title'], 'text': row['text']} for row in result]
+
+        print("-------------------------------------------------------------------------------------")
+        print(articles)
+        print("-------------------------------------------------------------------------------------")
+        # Return the list of titles and summaries
+        return articles
+
+
 
     @_catch_errors
     def add_url(self, url, recursive_depth=0, allow_dupes=False):
-        '''
-        Download the url, extract various metainformation, and add the metainformation into the db.
+        # '''
+        # Download the url, extract various metainformation, and add the metainformation into the db.
 
-        By default, the same url cannot be added into the database multiple times.
+        # By default, the same url cannot be added into the database multiple times.
 
-        >>> db = ArticleDB()
-        >>> db.add_url(ArticleDB._TESTURLS[0])
-        >>> db.add_url(ArticleDB._TESTURLS[0])
-        >>> db.add_url(ArticleDB._TESTURLS[0])
-        >>> len(db)
-        1
+        # >>> db = ArticleDB()
+        # >>> db.add_url(ArticleDB._TESTURLS[0])
+        # >>> db.add_url(ArticleDB._TESTURLS[0])
+        # >>> db.add_url(ArticleDB._TESTURLS[0])
+        # >>> len(db)
+        # 1
 
-        >>> db = ArticleDB()
-        >>> db.add_url(ArticleDB._TESTURLS[0], allow_dupes=True)
-        >>> db.add_url(ArticleDB._TESTURLS[0], allow_dupes=True)
-        >>> db.add_url(ArticleDB._TESTURLS[0], allow_dupes=True)
-        >>> len(db)
-        3
+        # >>> db = ArticleDB()
+        # >>> db.add_url(ArticleDB._TESTURLS[0], allow_dupes=True)
+        # >>> db.add_url(ArticleDB._TESTURLS[0], allow_dupes=True)
+        # >>> db.add_url(ArticleDB._TESTURLS[0], allow_dupes=True)
+        # >>> len(db)
+        # 3
 
-        '''
+        # '''
+        # from bs4 import BeautifulSoup
+        # import requests
+        # import metahtml
+        
         logging.info(f'add_url {url}')
 
         if not allow_dupes:
@@ -344,44 +403,47 @@ class ArticleDB:
         return row[0]
 
 
-# if __name__ == '__main__':
-#     import argparse
-#     parser = argparse.ArgumentParser(description=__doc__)
-#     parser.add_argument('--loglevel', default='warning')
-#     parser.add_argument('--db', default='ragnews.db')
-#     parser.add_argument('--recursive_depth', default=0, type=int)
-#     parser.add_argument('--add_url', help='If this parameter is added, then the program will not provide an interactive QA session with the database.  Instead, the provided url will be downloaded and added to the database.')
-#     args = parser.parse_args()
-
-#     logging.basicConfig(
-#         format='%(asctime)s %(levelname)-8s %(message)s',
-#         datefmt='%Y-%m-%d %H:%M:%S',
-#         level=args.loglevel.upper(),
-#         )
-
-#     db = ArticleDB(args.db)
-
-#     if args.add_url:
-#         db.add_url(args.add_url, recursive_depth=args.recursive_depth, allow_dupes=True)
-
-#     else:
-#         import readline
-#         while True:
-#             text = input('ragnews> ')
-#             if len(text.strip()) > 0:
-#                 output = rag(text, db)
-#                 print(output)
-
 if __name__ == '__main__':
     import argparse
-    
-    parser = argparse.ArgumentParser(description="Test the args of varios functions")
-    
-    parser.add_argument('Text', type=str, help="Text to extract the keywords" )
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--loglevel', default='warning')
+    parser.add_argument('--db', default='ragnews.db')
+    parser.add_argument('--recursive_depth', default=0, type=int)
+    parser.add_argument('--add_url', help='If this parameter is added, then the program will not provide an interactive QA session with the database.  Instead, the provided url will be downloaded and added to the database.')
     args = parser.parse_args()
+
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        level=args.loglevel.upper(),
+        )
+
+    db = ArticleDB(args.db)
+
+    if args.add_url:
+        db.add_url(args.add_url, recursive_depth=args.recursive_depth, allow_dupes=True)
+
+    else:
+        import readline
+        while True:
+            text = input('ragnews> ')
+            if len(text.strip()) > 0:
+                output = rag(text, db)
+                print(output)
+
+# if __name__ == '__main__':
+#     import argparse
     
-    text = args.Text
+#     parser = argparse.ArgumentParser(description="Test the args of varios functions")
     
-    print(extract_keywords(text,seed=0 ))
+    
+#     parser.add_argument('Text', type=str, help="Text to extract the keywords" )
+#     args = parser.parse_args()
+    
+#     text = args.Text
+    
+#     # print(extract_keywords(text,seed=0 ))
+#     print(rag(like))
+    
     
     
