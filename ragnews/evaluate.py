3c97a0e6897a3129bcac0208c0b52928e3e59e45
Alexander Wood <115967095+KentaWood@users.noreply.github.com>
2024-09-23 10:36:23 -0700

    added the valid_labels prompt, dynamically programmed based on the test_json file, ACC: %82.34

--- ragnews/evaluate.py
+++ ragnews/evaluate.py
@@ -3,7 +3,8 @@
 from cgitb import text
 from imghdr import tests
 import json
-
+from typing import List
+import logging
 import ragnews
 import argparse
 
@@ -17,7 +18,7 @@ class RAGEvaluator:
         '''
 		pass
 
-	def predict(self, maskedtext: str):
+	def predict(self, maskedtext: str, valid_labels: set()):
 		'''
 		>>> model = RAGEvaluator()
 		>>> model.predict('[MASK0] is the democratic nominee.')
@@ -35,6 +36,10 @@ class RAGEvaluator:
 		Explain your reasoning briefly in no more than 50 words. If the answer is a person, provide only their last name.
 		If the given no tokens say say ""
 		Only ever give the answer as the predicted words with a space in between and no other pireces of context,explainations or even comments
+  
+		I'm going to provide you a sentence, and that sentence will have a masked token inside of it that will look like [MASK0] or [MASK1] or [MASK2] and so on.
+		And your job is to tell me what the value of that masked token was.
+		Valid values include: {valid_labels}
 
 		Example:
 		Therefore [MASK0] is Sarah and [MASK1] is John.
@@ -66,8 +71,24 @@ class RAGEvaluator:
 
 
 	def accuracy(self, file_Json):
+		#the valid labels for the test set
+		labels = set()
+		
+		# list of the [{masked_text: text} , {masks: answers}] 
+		tests = []
+		
+  
 		with open(file_Json, 'r') as f:
-			tests = [json.loads(line) for line in f]
+			for line in f:
+				dp = json.loads(line)
+				labels.update(dp['masks'])
+				tests.append(dp)
+				
+			# tests = [json.loads(line) for line in f]
+		
+		print(labels)
+  
+		
 
 		# Initialize counters for total masks and correct predictions
 		total_masks = 0
@@ -75,10 +96,11 @@ class RAGEvaluator:
 
 		# For debugging, limit the number of tests (remove or adjust as needed)
 		# tests = tests[:5]
-
+		
+		print(tests)
 		for test in tests:
 			# Get the predicted masks from the model
-			pred = self.predict(test['masked_text'])
+			pred = self.predict(test['masked_text'],valid_labels=labels)
 
 		
 			actual_masks = test['masks']  # Actual masks from the file
@@ -110,6 +132,6 @@ if __name__ == '__main__':
     model = RAGEvaluator()
 
     # Call the accuracy function and pass the data file
-    print( "Accuracy of the is:" + model.accuracy(file_Json=args.data_file))
+    print( "Accuracy of the is:",model.accuracy(file_Json=args.data_file))
     
     
